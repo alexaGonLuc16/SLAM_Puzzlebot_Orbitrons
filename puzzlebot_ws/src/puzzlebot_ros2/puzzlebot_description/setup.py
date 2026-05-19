52cde39ac6a454bbe101657ae9a61110f9e0378f
Mario Godinez Chavero <marioqroqueretaro@outlook.com>
2026-05-19 10:13:24 -0600

    Revert "Merge changes into main"
    This reverts commit 68031bb93400408ba3ffe1dddb23981f0454d2e2, reversing
    changes made to 6c40c0eb5e5ed094ae6d1aead3ea775480c0d6a1.

---
+++ puzzlebot_ws/src/puzzlebot_ros2/puzzlebot_description/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'puzzlebot_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.stl'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aldemarg',
+    maintainer_email='pullos30@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'puzzlebot_transforms = puzzlebot_sim.transforms:main'
+        ],
+    },
+)
